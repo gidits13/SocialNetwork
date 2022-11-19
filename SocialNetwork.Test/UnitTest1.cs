225979d904c852c47fa220f4d2798cf6ec167627
gidits13 <gidits13@gmail.com>
2022-11-19 17:49:03 +0300

    Add Tests

---
+++ SocialNetwork.Test/UnitTest1.cs
@@ -0,0 +1,83 @@
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using NUnit.Framework;
+using SocialNetwork;
+using System;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.BLL.Services.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SocialNetwork.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void RegistrUserTest()
+        {
+            var userService = new UserService();
+            var userRegistrationData = new UserRegistrationData()
+            {
+                FirstName = "Pavel",
+                LastName = "Pavlov",
+                Email = "pavel@ya.ru",
+                Password = "40404040"
+            };
+
+            try
+            {
+                userService.Register(userRegistrationData);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void AddFriendTest()
+        {
+            var friendAdddata = new FriendAddData()
+            {
+                FriendEmail = "petr@ya.ru",
+                UserId = 1,
+            };
+            FriendService friendService = new FriendService();
+
+            try
+            {
+                friendService.Add(friendAdddata);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+            } 
+        }
+        [Test]
+        public void DeleteFriendTest()
+        {
+            var frienDeleteFData = new FriendDeleteData()
+            {
+                UserID = 1,
+                FriendEmail = "petr@ya.ru",
+                FrienId = 3,
+            };
+
+        var friendService = new FriendService();
+            try
+            {
+                friendService.Delete(frienDeleteFData);
+            }
+            catch (Exception)
+            {
+
+                Assert.Fail();
+            }
+        }
+    }
+}
